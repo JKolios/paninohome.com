57fc4ee6d2f92723923a32ee42bb2059f9c2055b
Jason Kolios <jason.kolios@workist.com>
2022-11-09 23:54:25 +0200

    Adds ESPA logo and info

---
+++ html/espa_fund.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="el" xml:lang="el">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+<meta name="description" content="ΠΑΝΙΝΟ, Θεμελίδου Ι. - Μπάνου Γ. ΟΕ, Είδη σπιτιού, δώρου, Το ιστορικό της εταιρείας μας" />
+<title>Paninohome.com - ΠΑΝΙΝΟ, Θεμελίδου Ι. - Μπάνου Γ. ΟΕ</title>
+
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<img src="images/panino.png" alt="Panino Logo" />
+<?php include("language_switch.php"); ?>
+<div id="mod-id" class="mod">
+
+   <div class="mod-content">
+     <div class="hd">
+         <?php include("nav.php"); ?>
+     </div>
+     <div class="bd">
+       <h2>Περιφέρεια Αττικής</h2>
+       <h2>Περιφερειακό Επιχειρησιακό Πρόγραμμα</h2>
+       <p>Επιχειρησιακό Πρόγραμμα: Αττική 2014-2020 Ενίσχυση επιχ/σεων Δήμου Ραφήνας-Πικερμίου πληγέντων απο τις πυρκαγιές τηε 23-07-2018</p>
+
+       <p>Δικαιούχος: Ι.Θεμελίδου-Γ.Μπάνου ΟΕ</p>
+      
+       <p>Ονομασία Πράξης: ΑΤΤΕ2-0046891</p>
+       
+       <p>Κύριος στόχος δραστηριότητας: Ενίσχυση μικρών επιχειρήσεων προκειμένου να συνεχίσουν την λειτουργία τους</p>
+
+     </div>
+     <div class="ft">
+        <?php include("foot.php"); ?>
+</div>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
+<script type="text/javascript">
+try{
+var pageTracker = _gat._getTracker("UA-6754715-1");
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
+</body>
+</html>
